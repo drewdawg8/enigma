deef7bdf52a4791da7b7965204f31f4f205a9550
dstewa17@masonlive.gmu.edu <dstewa17@masonlive.gmu.edu>
2019-06-17 14:29:09 -0700

    asadfasd

--- upsidedown/filesystem.py
+++ upsidedown/filesystem.py
@@ -1,13 +1,35 @@
 import fs
 
+class FileSystem:
 
-mem_fs = fs.open_fs('mem://')
-mem_fs.makedirs('fruit')
-mem_fs.makedirs('vegtables')
-mem_fs = fs.open_fs('mem://fruit')
-mem_fs.makedirs('apple')
-mem_fs = fs.open_fs('mem://')
-mem_fs.tree()
+    def __init__(self, rootfs):
+        temp = rootfs.makedirs('root')
+        self.__root = rootfs.opendir('root')
+        self.__rootfs = rootfs
+        self.__current = self.__root
+        self.__prev = None
 
+    #Makes a directory and sets it to the current directory
+    def mkdir(self, dir_name):
+        self.__current.makedirs(dir_name)
 
-def create_dir()
+    def cd(self, path):
+        self.__prev = self.__current
+        self.__current = self.__current.opendir(path)
+
+    def tree(self):
+        return self.__current.desc(self.__current))
+
+#fs = fs.open_fs('mem://')
+#fs.makedirs('root')
+#root = fs.opendir('/root')
+#root.makedirs('Documents')
+#root.makedirs('Pictures')
+#documents = root.opendir('Documents')
+#print(documents.desc('.'))
+#root.tree()
+
+fs = FileSystem(fs.open_fs('mem://'))
+fs.mkdir('Pictures')
+fs.cd('Pictures')
+print(fs.tree())
--- upsidedown/test.py
+++ upsidedown/test.py
@@ -1,4 +1,4 @@
 from rw import *
 
-write_to_drive(50000, 'Hello world!')
+write_to_drive(50000, 'Testing 1 2 3!')
 print(read_from_drive(50000, 512))
